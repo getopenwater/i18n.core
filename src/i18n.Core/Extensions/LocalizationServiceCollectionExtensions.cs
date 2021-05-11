d6e0a5478419ead33527637f3921f9216f638ee5
Alexey Bykov <alexey-bycov@yandex.ru>
2021-05-11 20:04:38 +0700

    Simplified the AddI18NLocalization() method

--- src/i18n.Core/Extensions/LocalizationServiceCollectionExtensions.cs
+++ src/i18n.Core/Extensions/LocalizationServiceCollectionExtensions.cs
@@ -20,47 +20,30 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <summary>
         /// Registers the services to enable localization using Portable Object files.
         /// </summary>
-        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-        /// <param name="hostEnvironment"></param>
-        /// <param name="requestLocalizationSetupAction">An action to configure the Microsoft.Extensions.Localization.RequestLocalizationOptions.</param>
-        /// <param name="middleWareOptionsSetupAction">An action to configure the i18n.Core.Middleware.I18NMiddlewareOptions</param>
         [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
         public static IServiceCollection AddI18NLocalization([JetBrains.Annotations.NotNull] this IServiceCollection services,
-            [JetBrains.Annotations.NotNull] IHostEnvironment hostEnvironment, Action<RequestLocalizationOptions> requestLocalizationSetupAction = null, Action<I18NMiddlewareOptions> middleWareOptionsSetupAction = null)
+            [JetBrains.Annotations.NotNull] IHostEnvironment hostEnvironment, Action<I18NMiddlewareOptions> middlewareOptionsSetupAction = null)
         {
-            if (services == null) throw new ArgumentNullException(nameof(services));
-            if (hostEnvironment == null) throw new ArgumentNullException(nameof(hostEnvironment));
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
 
-            var defaultPooledStreamManager = new DefaultPooledStreamManager();
+            if (hostEnvironment == null)
+                throw new ArgumentNullException(nameof(hostEnvironment));
 
             services.AddSingleton<INuggetReplacer, DefaultNuggetReplacer>();
             services.AddSingleton<ITranslationProvider, PortableObjectFilesTranslationsProvider>();
             services.AddSingleton<ILocalizationFilesProvider, PortableObjectFilesProvider>();
             services.AddSingleton<ILocalizationManager, LocalizationManager>();
-            services.AddSingleton<IPooledStreamManager>(defaultPooledStreamManager);
+            services.AddSingleton<IPooledStreamManager>(new DefaultPooledStreamManager());
+
+            services.Configure<I18NMiddlewareOptions>(x =>
+            {
+                x.CacheEnabled = !hostEnvironment.IsDevelopment();
+
+                middlewareOptionsSetupAction?.Invoke(x);
+            });
 
             services.AddOptions<I18NLocalizationOptions>().BindConfiguration(nameof(I18NLocalizationOptions));
-
-            if (requestLocalizationSetupAction != null)
-            {
-                services.Configure(requestLocalizationSetupAction);
-            }
-
-            if (middleWareOptionsSetupAction != null)
-            {
-                services.Configure<I18NMiddlewareOptions>(x =>
-                {
-                    x.CacheEnabled = !hostEnvironment.IsDevelopment();
-                    middleWareOptionsSetupAction(x);
-                });
-            }
-            else
-            {
-                services.AddSingleton(new I18NMiddlewareOptions
-                {
-                    CacheEnabled = !hostEnvironment.IsDevelopment()
-                });
-            }
 
             return services;
         }
